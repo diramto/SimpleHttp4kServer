e07d6f848f33ef253873c144f8bbbae89da81e16
ape06 <ape06@ox.ac.uk>
2025-04-07 15:49:54 +0100

    Cowboy Ivan

--- src/main/kotlin/com/oocode/Answerer.kt
+++ src/main/kotlin/com/oocode/Answerer.kt
@@ -3,7 +3,7 @@ package com.oocode
 class Answerer {
     fun answerFor(question: String): String {
         if (question.contains("name")) {
-            return "Paul"
+            return "Ivan"
         }
         if (question.contains("2")) {
             return "4"
